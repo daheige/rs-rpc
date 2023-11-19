6b2e937c40d64692605dd853b4fcd61748b7c7a4
daheige <zhuwei313@hotmail.com>
2023-11-19 12:40:15 +0800

    feat: update php grpc

--- clients/php/hello.php
+++ clients/php/hello.php
@@ -3,15 +3,7 @@ require dirname(__FILE__) . '/vendor/autoload.php';
 
 const GRPC_ADDRESS = '127.0.0.1:8081';
 
-/**
- * % php hello_client.php daheige123
- * check App\Grpc\GPBMetadata\Hello\HelloReq exist
- * bool(true)
- * status code: 0
- * name:hello,daheige123
- * call ok
- */
-
+// php hello.php daheige
 function greet($name)
 {
     $client = new App\Grpc\Hello\GreeterServiceClient(GRPC_ADDRESS, [
